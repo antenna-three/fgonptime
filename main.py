e43fd5dcb9533d32022c0b13731e492e70bddfd2
antenna-three <tkduc@outlook.com>
2021-05-14 02:16:03 +0900

    fix lack of target

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     servants = fetch_servants.main()
     servants = format_servants.main(servants)
     html = to_html.main(servants)
-    with open('public/index.html', 'w', encoding='utf-8'):
+    with open('public/index.html', 'w', encoding='utf-8') as f:
         f.write(html)
 
