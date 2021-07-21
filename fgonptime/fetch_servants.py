b7bfbefee61558cdde8ce35e12f755a434a15c11
antenna-three <tkduc@outlook.com>
2021-07-21 12:44:27 +0900

    Revert "Adapt fetching code to new table"
    This reverts commit a1caed1f7fc17f3d2b6a2e53decccecc0da56501.

--- fgonptime/fetch_servants.py
+++ fgonptime/fetch_servants.py
@@ -28,36 +28,40 @@ def fetch_hidden_status():
     fetch hidden status including noble phantasm time
     '''
     url = 'https://w.atwiki.jp/f_go/pages/304.html'
-    dfs = pandas.read_html(url, match='倍速/秒', header=0, index_col=0)
+    dfs = pandas.read_html(url, match='宝具長さ', header=(0, 1), index_col=0)
     if dfs:
         servants = dfs[0]
     else:
         raise IOError('Hidden status table not found')
+    servants.drop(index='No', inplace=True)
+    servants.drop(index='083', inplace=True) #drop solomon
     servants.rename(columns=str.lower, inplace=True)
-    servants.rename(columns={'クラス': 'class', '倍速/秒': 'strtime'}, inplace=True)
-    servants.dropna(axis='index', subset=['strtime'], inplace=True)
-    servants = servants.query('strtime not in ["不明", "省略"]')
+    servants.columns = ('_'.join(dict.fromkeys(c)) for c in servants.columns)
+    servants.rename(columns={'クラス': 'class', '宝具長さ_倍速': 'strtime'}, inplace=True)
     servants.columns.name = 'number'
+    servants = servants[~servants.index.duplicated(keep='first')]
     np_times = servants['strtime'].str.extractall(r'(?P<time>\d{1,2}\.\d)(?:[@＠](?P<cond>.{1,2}))?')
     min_times = np_times.astype({'time': 'float'}).sort_values('time').groupby(level=0).head(1).sort_index(0)
     min_times.reset_index(level=1, inplace=True)
     servants = pandas.concat([servants, min_times], axis=1)
-
-    np_split = servants['宝具'].str.extract('(?P<range>.*)(?P<color>[ＡＢＱABQ])')
-    servants = pandas.concat([servants, np_split], axis=1)
-    servants.replace({'color': {'Ａ': 'A', 'Ｂ': 'B', 'Ｑ': 'Q'}}, inplace=True)
     return servants
 
 def main():
     '''
-    extract required columns
+    merge servant list and hidden status
     '''
     with open('columns.json', 'r', encoding='utf-8') as f:
         columns = json.load(f)
 
-    #servants = fetch_ordered_servants()
-    servants = fetch_hidden_status()
-    return servants[columns]
+    servants = fetch_ordered_servants()
+    hidden_status = fetch_hidden_status()
+    merged = servants.merge(
+                hidden_status,
+                left_index=True,
+                right_index=True,
+                suffixes=(None, '_hidden')
+                )
+    return merged[columns]
 
 def dump_json():
     servants = main()
